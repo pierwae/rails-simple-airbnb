042438bc563c66ae1f71f99cce6826c342a3a3b4
Pierre Waechter <pierre.waechter@audencia.com>
2020-08-12 17:00:24 +0200

    css wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_133017) do
+ActiveRecord::Schema.define(version: 2020_08_12_145225) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_133017) do
     t.integer "number_of_guests"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
   end
 
 end
